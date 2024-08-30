0c26de8c4bcc611ca4d14b5ae33fe7d30111cec2
Marwin Kirchhofs <m.c.kirchhofs@student.tudelft.nl>
2024-08-30 18:08:12 +0200

    WIP - project-type specific sim makefile and make vars

---
+++ var.mk
@@ -0,0 +1,61 @@
+
+ifndef _VAR_MAKE_XILINX_
+_VAR_MAKE_XILINX_ := 1
+
+DIR_VAR_MAKE_XILINX		:= $(abspath $(dir $(lastword ${MAKEFILE_LIST})))
+
+include ${DIR_VAR_MAKE_XILINX}/../var.mk
+
+# TODO: this needs to go somewhere in the xilinx-respective scripts (maybe 
+# a make var there that gets included by this one?)
+XIL_TOOL 				:= vivado
+XIL_PRJ_NAME			:= ${PRJ_NAME}
+DIR_XIL_PRJ				:= ${DIR_PRJ_TOP}/_vivado_prj
+XIL_PRJ					:= ${DIR_XIL_PRJ}/${XIL_PRJ_NAME}.xpr
+
+DIR_XIL_PRJ_XIPS		:= ${DIR_XIL_PRJ}/${XIL_PRJ_NAME}.srcs/sources_1/ip
+
+DIR_XIPS        		:= ${DIR_PRJ_TOP}/xips
+DIR_SCRIPTS_XIL 		:= ${DIR_PRJ_TOP}/scripts_xil
+DIR_XIP_CTRL			:= ${DIR_PRJ_TOP}/xip_ctrl
+
+SCRIPT_CREATE_PROJECT	:= ${DIR_SCRIPTS_XIL}/create_project.tcl
+SCRIPT_MANAGE_PROJECT	:= ${DIR_SCRIPTS_XIL}/manage_project.tcl
+SCRIPT_READ_SOURCES		:= ${DIR_SCRIPTS_XIL}/read_sources.tcl
+SCRIPT_SOURCE_HELPERS	:= ${DIR_SCRIPTS_XIL}/source_helper_scripts.tcl
+SCRIPT_GENERATE_XIPS	:= ${DIR_SCRIPTS_XIL}/generate_xips.tcl
+SCRIPT_BUILD_HW			:= ${DIR_SCRIPTS_XIL}/build_hw.tcl
+ifneq (,$(wildcard ${DIR_XIP_CTRL}/${VIO_CTRL_TOP}_vio_ctrl.tcl))
+SCRIPT_VIO_CTRL			:= ${DIR_XIP_CTRL}/${VIO_CTRL_TOP}_vio_ctrl.tcl
+else
+SCRIPT_VIO_CTRL			:= ${DIR_SCRIPTS_XIL}/vio_ctrl.tcl
+endif
+
+##############################
+# IP SOURCES
+##############################
+
+# TODO: this way, LIST_XIPS will also contain old IPs which are not present in 
+# the project anymore. I mean, in that case they also shouldn't really be in the 
+# vivado directory anymore, so the fix is much more that at any update, you 
+# remove the sources for the IPs that are not needed anymore
+LIST_XIPS				:= $(shell ls ${DIR_XIL_PRJ_XIPS} 2>/dev/null)
+LIST_XIPS_XCI			:= $(wildcard ${DIR_XIL_PRJ_XIPS}/*/*.xci)
+DIR_XIPS_SIM_OUT		:= ${DIR_SIM}/${SIMULATOR}/xip_sim_export
+# list of all xips with exported simulation scripts (this one is accurate as 
+# opposed to LIST_XIPS, because the directories that are present actually origin 
+# from the project (which gets cleaned before every export))
+# TODO: come up with something that causes this thing not to complain if the 
+# directory is not there, and instead just be empty
+LIST_XIPS_SIM			= $(shell ls ${DIR_XIPS_SIM_OUT} 2> /dev/null)
+DIR_XIL_IP_PRECOMPILE	:= $(call fun_get_prj_config_var,xil_ip_precompile_path)/${SIMULATOR}
+
+##############################
+# DEBUG CORES
+##############################
+
+XIL_DEBUG_CORE_FILES := $(addsuffix .tcl,                                   \
+                            $(addprefix ${DIR_XIPS}/xips_debug_cores_,${SRC_MODULES}))
+
+
+endif
